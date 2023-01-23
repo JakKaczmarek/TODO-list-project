adea88bbc29096c365626fec64065d9db4d6867c
JakKaczmarek <jakub.kaczmarek1114@gmail.com>
2023-01-24 00:31:41 +0100

    Added&edited closeCircle icon to TodoList.js and App.css

--- src/components/TodoList.js
+++ src/components/TodoList.js
@@ -1,5 +1,14 @@
 import React from "react";
+import { RiCloseCircleLine } from "react-icons/ri";
 
 export default function TodoList(props) {
-  return <div className="todo-row">{props.todo.text}</div>;
+  return (
+    <div className="todo-row">
+      {props.todo.text}
+      <div className="icon-container">
+        <RiCloseCircleLine className="icon" />
+      </div>
+    </div>
+  );
 }
+// CZEMU NIE DZIAŁA I WYWALA BŁAD PODCZAS SCIĄGANIA IKONKI
